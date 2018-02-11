d864a459bb4ffb32aa79e10c6cb4a6e790b08fed
smx <995047027@qq.com>
2018-02-11 11:42:12 +0800

    2018 02 11

---
+++ MyPractises/Sockets/SocketServer/Converters/EnableConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SocketServer.Converters
+{
+    public class EnableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                bool isOpen = (bool)value;
+                return !isOpen;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MyPractises/Sockets/SocketServer/ViewModels/MainWindowViewModel.cs
+++ MyPractises/Sockets/SocketServer/ViewModels/MainWindowViewModel.cs
@@ -7,6 +7,10 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using LogLibrary;
+using System.Threading;
 
 namespace SocketServer.ViewModels
 {
@@ -88,6 +92,21 @@ namespace SocketServer.ViewModels
             }
         }
 
+
+
+        private string rcvMsg;
+        public string RcvMessage
+        {
+            get { return rcvMsg; }
+            set
+            {
+                rcvMsg = value;
+                this.RaisePropertyChanged("RcvMessage");
+            }
+        }
+
+
+
         void OpenSocket()
         {
             try
@@ -97,11 +116,14 @@ namespace SocketServer.ViewModels
                 serverSock.Bind(ip);
                 serverSock.Listen(10);
                 IsSocketOpened = true;
+                AppendMessage($"Open Socket Successfully! Ip:{ServerIp}, Port:{ServerPort}");
+                StartListen();
             }
             catch (Exception ex)
             {
                 //LogWriter.Instance.ActionLogger.Error(ex.Message);
                 IsSocketOpened = false;
+                AppendMessage($"Open Socket Failed! Ip:{ServerIp}, Port:{ServerPort}");
             }
            
         }
@@ -111,14 +133,17 @@ namespace SocketServer.ViewModels
         {
             try
             {
+                //serverSock.Shutdown(SocketShutdown.Both);
+                //Thread.Sleep(10);
                 serverSock.Close();
+                AppendMessage($"Close Socket Successfully! Ip:{ServerIp}, Port:{ServerPort}");
+                IsSocketOpened = false;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                
+                AppendMessage($"Close Socket Failed! Ip:{ServerIp}, Port:{ServerPort}");
+                LogHelper.Log("Close Socket Failed! msg:{0} pos:{1}", ex.Message, ex.StackTrace);
             }
-            IsSocketOpened = false;
         }
 
 
@@ -127,5 +152,53 @@ namespace SocketServer.ViewModels
             return !IsSocketOpened;
         }
 
+        void AppendMessage(string message)
+        {
+            RcvMessage += message + "\n";
+        }
+
+        async void StartListen()
+        {
+            await Task.Run(()=> 
+            {
+                while (IsSocketOpened)
+                {
+                    try
+                    {
+                        //判断是否被释放
+                       
+                        Socket clientSock = serverSock.Accept();
+                        AppendMessage($"client {clientSock.RemoteEndPoint}  Message:Connected");
+                        byte[] buffer = new byte[1024];
+                        bool isEndSocket = false;
+                        while (!isEndSocket)
+                        {
+                            int rcvLen = clientSock.Receive(buffer);
+                            if (0 != rcvLen)
+                            {
+                                string message = Encoding.ASCII.GetString(buffer, 0, rcvLen);
+                                AppendMessage($"client {clientSock.RemoteEndPoint}  Message:{message}");
+                            }
+                            else
+                            {
+                                isEndSocket = true;
+                                AppendMessage($"client {clientSock.RemoteEndPoint}  Message:Disconnect");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        LogHelper.Log("Accept err:" + ex.Message);
+                        LogHelper.Log("pos:" + ex.StackTrace);
+                    }
+                }
+               
+            });
+           
+        }
+
     }
+
+
+    
 }
